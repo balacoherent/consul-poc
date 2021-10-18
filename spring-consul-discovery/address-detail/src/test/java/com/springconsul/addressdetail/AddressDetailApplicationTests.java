13a4cfe7980b5b4ee9122f85575f4008585cebdc
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-10-18 19:19:37 +0530

    Add files via upload

---
+++ spring-consul-discovery/address-detail/src/test/java/com/springconsul/addressdetail/AddressDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springconsul.addressdetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
